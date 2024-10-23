867e6304d97be00b9184d49d1a24769eeb7a53f6
Mohit Kumar <mohitmpib@gmail.com>
2024-10-23 13:21:11 +0530

    update

---
+++ Hooks/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import "./App.css"
+import UseStateHook from './UseStateHook'
+import BgColor from './BgColor'
+
+function App() {
+  
+
+  return (
+    <>
+    {/* <UseStateHook/> */}
+     <BgColor/>
+    </>
+  )
+}
+
+export default App
