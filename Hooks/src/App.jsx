b466a31068c6f178b3666d35b69f78ca3aa7d03d
Mohit Kumar <mohitmpib@gmail.com>
2024-11-06 14:43:22 +0530

    useEffect

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -3,16 +3,28 @@ import React from 'react'
 import "./App.css"
 import UseStateHook from './UseStateHook'
 import BgColor from './BgColor'
+// import UseRef from './UseRef'
+import UseRefTask from './UseRefTask'
+import UseRef1 from './UseRef1'
+import UseEffect from './UseEffect'
+import UseEffectTask from './UseEffectTask'
 
 function App() {
   
 
   return (
     <>
-    {/* <UseStateHook/> */}
-     <BgColor/>
+      {/* <UseStateHook/> */}
+      {/* <BgColor/> */}
+      {/* <UseRef/> */}
+      {/* 
+      <UseRefTask />
+      <UseRef1></UseRef1> */}
+
+      {/* <UseEffect/> */}
+      <UseEffectTask/>
     </>
-  )
+  );
 }
 
 export default App
